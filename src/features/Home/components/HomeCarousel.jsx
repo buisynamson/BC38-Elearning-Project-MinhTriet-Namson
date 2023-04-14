7d84532731d8aacfb2f04279a979e2ad7bb4ed24
Son Bui <buisynamson@gmail.com>
2023-04-14 07:27:27 +0700

    fix carousel

--- src/features/Home/components/HomeCarousel.jsx
+++ src/features/Home/components/HomeCarousel.jsx
@@ -5,13 +5,13 @@ const HomeCarousel = () => {
   return (
     <Carousel autoplay effect="fade">
       <div>
-        <img src="./img/63898019af47c60040e48ea7.jpg" alt="" />
+        <img className='w-full' src="./img/63898019af47c60040e48ea7.jpg" alt="" />
       </div>
       <div>
-        <img src="./img/63ac0c9ef5c9150025b52903.jpg" alt="" />
+        <img className='w-full' src="./img/63ac0c9ef5c9150025b52903.jpg" alt="" />
       </div>
       <div>
-        <img src="./img/63b68ce8d45fde00262e81cd.jpg" alt="" />
+        <img className='w-full' src="./img/63b68ce8d45fde00262e81cd.jpg" alt="" />
       </div>
     </Carousel>
   );
